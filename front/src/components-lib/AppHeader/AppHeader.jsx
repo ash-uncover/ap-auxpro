28ac3a119ea0ff933edea0819823ad22b3ea2c2d
aSH <ash.uncover@gmail.com>
2017-08-26 14:58:47 +0200

    factorized app header

--- front/src/components-lib/AppHeader/AppHeader.jsx
+++ front/src/components-lib/AppHeader/AppHeader.jsx
@@ -21,105 +21,72 @@ class AppHeader extends React.Component {
 		AppHeaderData.unregister()
 	}
 
-    _buildText(text, type) { 
-        return (
-            <Navbar.Text 
-                text={text} 
-                active={(AuthHelper.getType() || '') === type} />
-        )
-    }
+	_buildText(text, type) { 
+		return (
+			<Navbar.Text 
+				text={text} 
+				active={this.state.authType === type} />
+		)
+	}
 
 	_buildLink(text, link) { 
-        return (
-            <Navbar.Link 
-                text={text} 
-                link={link} 
-                onNavigate={AppHelper.navigate} 
-                active={(AppHelper.getData('/path') || '').startsWith(link)} />
-        )
-    }
+		return (
+			<Navbar.Link 
+				text={text} 
+				link={link} 
+				onNavigate={AppHelper.navigate} 
+				active={(AppHelper.getData('/path') || '').startsWith(link)} />
+		)
+	}
 
-	render() {
+	_buildLinks() { 
 		switch (this.state.authType) {
-			case 'auxiliary': return this.renderAuxiliary()
-			case 'service': return this.renderService()
-			default: return this.renderDefault()
+			case 'auxiliary': return (
+				<Navbar.Group right>
+					{this._buildLink('Accueil','/auxiliary/home')}
+					{this._buildLink('Profil','/auxiliary/infos')}
+					{this._buildLink('Planing','/auxiliary/planing')}
+					{this._buildLink('Zone','/auxiliary/zone')}
+					{this._buildLink('Offres','/auxiliary/offers')}
+					{this._buildLink('Déconnexion','/logout')}
+				</Navbar.Group>
+			)
+			case 'service': return (
+				<Navbar.Group right>
+					{this._buildLink('Accueil','/service/home')}
+					{this._buildLink('Profil','/service/infos')}
+					{this._buildLink('Zone','/service/zone')}
+					{this._buildLink('Usagers','/service/customers')}
+					{this._buildLink('Interventions','/service/interventions')}
+					{this._buildLink('Déconnexion','/logout')}
+				</Navbar.Group>
+			)
+			default: return (
+				<Navbar.Group right>
+					{this._buildLink('Connexion','/auth/login')}
+					{this._buildLink("S'inscrire",'/auth/register')}
+				</Navbar.Group>
+			)
 		}
 	}
 
-	renderDefault() {
+	render() {
 		return (
-            <header className='hidden-print ap-app-header'>
-                <Navbar fixedTop>
-                	<Navbar.Header 
-                        brandText='AuXpros'
-                        brandImage='assets/images/auxpro-logo.png'
-                        brandLink='/home'
-                        onNavigate={AppHelper.navigate} />
-                    <Navbar.Group>
-                        {this._buildText('AUX','auxiliary')}
-                        {this._buildText('SAD','service')}
-                    </Navbar.Group>
-                    <Navbar.Group right>
-                        {this._buildLink('Connexion','/auth/login')}
-                        {this._buildLink("S'inscrire",'/auth/register')}
-                    </Navbar.Group>
-                </Navbar>
-            </header>
-        )
+			<header className='hidden-print ap-app-header'>
+				<Navbar fixedTop>
+					<Navbar.Header 
+						brandText='AuXpros'
+						brandImage='assets/images/auxpro-logo.png'
+						brandLink='/home'
+						onNavigate={AppHelper.navigate} />
+					<Navbar.Group>
+						{this._buildText('AUX','auxiliary')}
+						{this._buildText('SAD','service')}
+					</Navbar.Group>
+					{this._buildLinks()}
+				</Navbar>
+			</header>
+		)
 	}
-
-	renderAuxiliary() {
-		return (
-            <header className='hidden-print ap-app-header'>
-                <Navbar fixedTop>
-               		<Navbar.Header 
-                        brandText='AuXpros'
-                        brandImage='assets/images/auxpro-logo.png'
-                        brandLink='/home'
-                        onNavigate={AppHelper.navigate} />
-                    <Navbar.Group>
-                        {this._buildText('AUX','auxiliary')}
-                        {this._buildText('SAD','service')}
-                    </Navbar.Group>
-                    <Navbar.Group right>
-                        {this._buildLink('Accueil','/auxiliary/home')}
-                        {this._buildLink('Profil','/auxiliary/infos')}
-                        {this._buildLink('Planing','/auxiliary/planing')}
-                        {this._buildLink('Zone','/auxiliary/zone')}
-                        {this._buildLink('Offres','/auxiliary/offers')}
-                        {this._buildLink('Déconnexion','/logout')}
-                    </Navbar.Group>
-                </Navbar>
-            </header>
-        )
-	}
-
-	renderService() {
-		return (
-            <header className='hidden-print ap-app-header'>
-                <Navbar fixedTop>
-                	<Navbar.Header 
-                        brandText='AuXpros'
-                        brandImage='assets/images/auxpro-logo.png'
-                        brandLink='/home'
-                        onNavigate={AppHelper.navigate} />
-                    <Navbar.Group>
-                        {this._buildText('AUX','auxiliary')}
-                        {this._buildText('SAD','service')}
-                    </Navbar.Group>
-                    <Navbar.Group right>
-                        {this._buildLink('Accueil','/service/home')}
-                        {this._buildLink('Profil','/service/infos')}
-                        {this._buildLink('Zone','/service/zone')}
-                        {this._buildLink('Usagers','/service/customers')}
-                        {this._buildLink('Interventions','/service/interventions')}
-                        {this._buildLink('Déconnexion','/logout')}
-                    </Navbar.Group>
-                </Navbar>
-            </header>
-        )
-	}
-
 }
 export default AppHeader
