37fd19f5d14065fc8b992591c9cfd6421a7cdd26
matthieu <matthieu@mail.com>
2017-10-08 22:14:51 +0200

    acceuil auxiliary

--- front/src/components/auxiliary/home/AuxiliaryHome.jsx
+++ front/src/components/auxiliary/home/AuxiliaryHome.jsx
@@ -22,13 +22,50 @@ class AuxiliaryHome extends React.Component {
 		return (
 			<Panel className='ap-auxiliary-home'>
 				<Panel.Header>
-					Auxiliary Home Header
+					Statut Compte
 				</Panel.Header>
 				<Panel.Body>
-					Auxiliary Home Body
+					{ AuxiliaryHomeData.premium 
+						? <strong>Compte Premium</strong> 
+						: <div className='ap-error'>Accès limité ! Passez votre compte en Premium pour profiter pleinement de nos fonctionnalités</div>
+					}
 				</Panel.Body>
 				<Panel.Footer>
-					Auxiliary Home Footer
+				</Panel.Footer>
+				<Panel.Header>
+					Statut profil
+				</Panel.Header>
+				<Panel.Body>
+					<strong>{'Votre profil est ' + (AuxiliaryHomeData.complete ? 'actif' : 'incomplet') + '.'}</strong>
+				</Panel.Body>
+				<Panel.Footer>
+				</Panel.Footer>
+				<Panel.Header>
+					Offres
+				</Panel.Header>
+				<Panel.Body>
+					<div><strong><b>{AuxiliaryHomeData.offersPendingCount}</b></strong>{AuxiliaryHomeData.textPending}</div>
+					<div><strong><b>{AuxiliaryHomeData.offersConfirmedCount}</b></strong>{AuxiliaryHomeData.textConfirmed}</div>
+					<div><strong><b>{AuxiliaryHomeData.offersRejectedCount}</b></strong>{AuxiliaryHomeData.textRejected}</div>
+					<div><strong><b>{AuxiliaryHomeData.offersCanceledCount}</b></strong>{AuxiliaryHomeData.textCanceled}</div>
+				</Panel.Body>
+				<Panel.Footer>
+				</Panel.Footer>
+				<Panel.Header>
+					Interventions
+				</Panel.Header>
+				<Panel.Body>
+					<div><strong><b>{AuxiliaryHomeData.interventionsCount}</b></strong>{AuxiliaryHomeData.textIntervention}</div>
+				</Panel.Body>
+				<Panel.Footer>
+				</Panel.Footer>
+				<Panel.Header>
+					Usagers
+				</Panel.Header>
+				<Panel.Body>
+					<div><strong><b>{AuxiliaryHomeData.customersCount}</b></strong>{AuxiliaryHomeData.textCustomer}</div>
+				</Panel.Body>
+				<Panel.Footer>
 				</Panel.Footer>
 			</Panel>
 		)
--- front/src/components/auxiliary/home/AuxiliaryHomeData.js
+++ front/src/components/auxiliary/home/AuxiliaryHomeData.js
@@ -1,8 +1,17 @@
 import AppHelper from 'helpers/AppHelper'
 import AuthHelper from 'helpers/AuthHelper'
 import AuxiliaryHelper from 'helpers/AuxiliaryHelper'
+import OfferHelper from 'helpers/OfferHelper'
+import CustomerHelper from 'helpers/CustomerHelper'
+import InterventionHelper from 'helpers/InterventionHelper'
+import MissionHelper from 'helpers/MissionHelper'
 
-import { BaseData } from 'ap-react-bootstrap'
+import AccountType from 'utils/constants/AccountType'
+import OfferStatusSad from 'utils/constants/OfferStatusSad'
+import AuxiliaryUtils from 'utils-lib/entities/AuxiliaryUtils'
+import InterventionUtils from 'utils-lib/entities/InterventionUtils'
+
+import { BaseData, Utils, TextUtils } from 'ap-react-bootstrap'
 
 class AuxiliaryHomeData extends BaseData {
 
@@ -11,19 +20,41 @@ class AuxiliaryHomeData extends BaseData {
 
 		this.obj.state = {}
 
-		AuxiliaryHelper.register(AuthHelper.getEntityId(), this, this._onAuxiliaryUpdate.bind(this))
+		let auxiliary = AuxiliaryHelper.getData(AuthHelper.getEntityId())
+		
+		this.premium = (auxiliary.accountType || '') === AccountType.PREMIUM.key
+		this.complete = AuxiliaryUtils.checkProfileCompleted(auxiliary)
+
+		let offers = Utils.map(OfferHelper.getData())
+		this.offersPendingCount = Utils.filter(offers, this.filterOffers.bind(this, OfferStatusSad.PENDING)).length
+		this.offersConfirmedCount = Utils.filter(offers, this.filterOffers.bind(this, OfferStatusSad.CONFIRMED)).length
+		this.offersRejectedCount = Utils.filter(offers, this.filterOffers.bind(this, OfferStatusSad.REJECTED)).length
+		this.offersCanceledCount = Utils.filter(offers, this.filterOffers.bind(this, OfferStatusSad.CANCELED)).length
+
+		this.textPending = ' ' + TextUtils.pluralize('nouvelle', this.offersPendingCount) + ' ' + TextUtils.pluralize('offre', this.offersPendingCount) + '.'
+		this.textConfirmed = ' ' + TextUtils.pluralize('offre', this.offersConfirmedCount) + ' ' + TextUtils.pluralize('confirmée', this.offersConfirmedCount) + '.'
+		this.textRejected = ' ' + TextUtils.pluralize('offre', this.offersRejectedCount) + ' ' + TextUtils.pluralize('rejetée', this.offersRejectedCount) + '.'
+		this.textCanceled = ' ' + TextUtils.pluralize('offre', this.offersCanceledCount) + ' ' + TextUtils.pluralize('annulée', this.offersCanceledCount) + '.'
+
+		this.customersCount = Utils.map(CustomerHelper.getData()).length
+		this.textCustomer = ' ' + TextUtils.pluralize('usager', this.customersCount ) + '.'
+
+		this.interventionsCount = Utils.filter(InterventionHelper.getData(), this.filterInterventions).length
+		this.textIntervention = ' ' + TextUtils.pluralize('intervention', this.interventionsCount) + ' en cours.'
+		
+		this.missionsCount = Utils.map(MissionHelper.getData()).length
 	}
 
 	unregister() {
-		AuxiliaryHelper.unregister(this)
 	}
 
-	_onAuxiliaryUpdate() {
-		let auxiliary = AuxiliaryHelper.getData(AuthHelper.getEntityId())
-		this.setState({
-		})
+	filterInterventions(intervention) {
+		return intervention.auxiliaryId === AuthHelper.getEntityId() && InterventionUtils.isActive(intervention)
 	}
 
+	filterOffers(sadStatus, offer) {
+		return !offer.hideToAux && OfferStatusSad.get(offer.sadStatus) === sadStatus
+	}
 }
 var AuxiliaryHomeObj = new AuxiliaryHomeData()
 export default AuxiliaryHomeObj
