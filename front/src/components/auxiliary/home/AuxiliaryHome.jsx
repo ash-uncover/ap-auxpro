97344967d058a080f0bcdb20d3c500d6881374fe
I051486 <antoine.maillard@sap.com>
2017-10-09 08:24:50 +0200

    home tiles

--- front/src/components/auxiliary/home/AuxiliaryHome.jsx
+++ front/src/components/auxiliary/home/AuxiliaryHome.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AuxiliaryHomeData from './AuxiliaryHomeData'
 import './AuxiliaryHome.scss'
 
-import { Panel } from 'ap-react-bootstrap'
+import { Panel, Grid } from 'ap-react-bootstrap'
 
 class AuxiliaryHome extends React.Component {
 
@@ -20,54 +20,75 @@ class AuxiliaryHome extends React.Component {
 
 	render() {  
 		return (
-			<Panel className='ap-auxiliary-home'>
-				<Panel.Header>
-					Statut Compte
-				</Panel.Header>
-				<Panel.Body>
-					{ AuxiliaryHomeData.premium 
-						? <strong>Compte Premium</strong> 
-						: <div className='ap-error'>Accès limité ! Passez votre compte en Premium pour profiter pleinement de nos fonctionnalités</div>
-					}
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Statut profil
-				</Panel.Header>
-				<Panel.Body>
-					<strong>{'Votre profil est ' + (AuxiliaryHomeData.complete ? 'actif' : 'incomplet') + '.'}</strong>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Offres
-				</Panel.Header>
-				<Panel.Body>
-					<div><strong><b>{AuxiliaryHomeData.offersPendingCount}</b></strong>{AuxiliaryHomeData.textPending}</div>
-					<div><strong><b>{AuxiliaryHomeData.offersConfirmedCount}</b></strong>{AuxiliaryHomeData.textConfirmed}</div>
-					<div><strong><b>{AuxiliaryHomeData.offersRejectedCount}</b></strong>{AuxiliaryHomeData.textRejected}</div>
-					<div><strong><b>{AuxiliaryHomeData.offersCanceledCount}</b></strong>{AuxiliaryHomeData.textCanceled}</div>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Interventions
-				</Panel.Header>
-				<Panel.Body>
-					<div><strong><b>{AuxiliaryHomeData.interventionsCount}</b></strong>{AuxiliaryHomeData.textIntervention}</div>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Usagers
-				</Panel.Header>
-				<Panel.Body>
-					<div><strong><b>{AuxiliaryHomeData.customersCount}</b></strong>{AuxiliaryHomeData.textCustomer}</div>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-			</Panel>
+			<Grid.Row className='ap-auxiliary-home'>
+				<Grid.Col sm={4} md={3}>
+					<Panel>
+						<Panel.Header>
+							Statut Compte
+						</Panel.Header>
+						<Panel.Body>
+							{ AuxiliaryHomeData.premium 
+								? <strong>Compte Premium</strong> 
+								: <div className='ap-error'>Accès limité ! Passez votre compte en Premium pour profiter pleinement de nos fonctionnalités</div>
+							}
+						</Panel.Body>
+						<Panel.Footer>
+						</Panel.Footer>
+					</Panel>
+				</Grid.Col>
+				<Grid.Col sm={4} md={3}>
+					<Panel>
+						<Panel.Header>
+							Statut profil
+						</Panel.Header>
+						<Panel.Body>
+							<strong>{'Votre profil est ' + (AuxiliaryHomeData.complete ? 'actif' : 'incomplet') + '.'}</strong>
+						</Panel.Body>
+						<Panel.Footer>
+						</Panel.Footer>
+					</Panel>
+				</Grid.Col>
+				<Grid.Clearfix />
+				<Grid.Col sm={4} md={3}>
+					<Panel>
+						<Panel.Header>
+							Offres
+						</Panel.Header>
+						<Panel.Body>
+							<div><strong><b>{AuxiliaryHomeData.offersPendingCount}</b></strong>{AuxiliaryHomeData.textPending}</div>
+							<div><strong><b>{AuxiliaryHomeData.offersConfirmedCount}</b></strong>{AuxiliaryHomeData.textConfirmed}</div>
+							<div><strong><b>{AuxiliaryHomeData.offersRejectedCount}</b></strong>{AuxiliaryHomeData.textRejected}</div>
+							<div><strong><b>{AuxiliaryHomeData.offersCanceledCount}</b></strong>{AuxiliaryHomeData.textCanceled}</div>
+						</Panel.Body>
+						<Panel.Footer>
+						</Panel.Footer>
+					</Panel>
+				</Grid.Col>
+				<Grid.Col sm={4} md={3}>
+					<Panel>
+						<Panel.Header>
+							Interventions
+						</Panel.Header>
+						<Panel.Body>
+							<div><strong><b>{AuxiliaryHomeData.interventionsCount}</b></strong>{AuxiliaryHomeData.textIntervention}</div>
+						</Panel.Body>
+						<Panel.Footer>
+						</Panel.Footer>
+					</Panel>
+				</Grid.Col>
+				<Grid.Col sm={4} md={3}>
+					<Panel>
+						<Panel.Header>
+							Usagers
+						</Panel.Header>
+						<Panel.Body>
+							<div><strong><b>{AuxiliaryHomeData.customersCount}</b></strong>{AuxiliaryHomeData.textCustomer}</div>
+						</Panel.Body>
+						<Panel.Footer>
+						</Panel.Footer>
+					</Panel>
+				</Grid.Col>
+			</Grid.Row>
 		)
 	}
 }
--- front/src/components/service/home/ServiceHome.jsx
+++ front/src/components/service/home/ServiceHome.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ServiceHomeData from './ServiceHomeData'
 import './ServiceHome.scss'
 
-import { Panel } from 'ap-react-bootstrap'
+import { Panel, Grid } from 'ap-react-bootstrap'
 
 class ServiceHome extends React.Component {
 
@@ -20,51 +20,72 @@ class ServiceHome extends React.Component {
 
 	render() {
 		return (
-			<Panel className='ap-service-home'>
-				<Panel.Header>
-					Statut Compte
-				</Panel.Header>
-				<Panel.Body>
-					{ ServiceHomeData.premium 
-						? <strong>Compte Premium</strong> 
-						: <div className='ap-error'>Accès limité ! Passez votre compte en Premium pour profiter pleinement de nos fonctionnalités</div>
-					}
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Statut profil
-				</Panel.Header>
-				<Panel.Body>
-					<strong>{'Votre profil est ' + (ServiceHomeData.complete ? 'actif' : 'incomplet') + '.'}</strong>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Usagers
-				</Panel.Header>
-				<Panel.Body>
-					<div><strong><b>{ServiceHomeData.customersCount}</b></strong>{ServiceHomeData.textCustomer}</div>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Interventions
-				</Panel.Header>
-				<Panel.Body>
-					<div><strong><b>{ServiceHomeData.interventionsCount}</b></strong>{ServiceHomeData.textIntervention}</div>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-				<Panel.Header>
-					Auxiliaires
-				</Panel.Header>
-				<Panel.Body>
-					<div><strong><b>{ServiceHomeData.auxiliariesCount}</b></strong>{ServiceHomeData.textauxiliaries}</div>
-				</Panel.Body>
-				<Panel.Footer>
-				</Panel.Footer>
-			</Panel>
+            <Grid.Row className='ap-service-home'>
+                <Grid.Col sm={4} md={3}>
+        			<Panel>
+        				<Panel.Header>
+        					Statut Compte
+        				</Panel.Header>
+        				<Panel.Body>
+        					{ ServiceHomeData.premium 
+        						? <strong>Compte Premium</strong> 
+        						: <div className='ap-error'>Accès limité ! Passez votre compte en Premium pour profiter pleinement de nos fonctionnalités</div>
+        					}
+        				</Panel.Body>
+        				<Panel.Footer>
+        				</Panel.Footer>
+                    </Panel>
+                </Grid.Col>
+                <Grid.Col sm={4} md={3}>
+                    <Panel>
+        				<Panel.Header>
+        					Statut profil
+        				</Panel.Header>
+        				<Panel.Body>
+        					<strong>{'Votre profil est ' + (ServiceHomeData.complete ? 'actif' : 'incomplet') + '.'}</strong>
+        				</Panel.Body>
+        				<Panel.Footer>
+        				</Panel.Footer>
+                    </Panel>
+                </Grid.Col>
+                <Grid.Clearfix />
+                <Grid.Col sm={4} md={3}>
+                    <Panel>
+        				<Panel.Header>
+        					Usagers
+        				</Panel.Header>
+        				<Panel.Body>
+        					<div><strong><b>{ServiceHomeData.customersCount}</b></strong>{ServiceHomeData.textCustomer}</div>
+        				</Panel.Body>
+        				<Panel.Footer>
+        				</Panel.Footer>
+                    </Panel>
+                </Grid.Col>
+                <Grid.Col sm={4} md={3}>
+                    <Panel>
+        				<Panel.Header>
+        					Interventions
+        				</Panel.Header>
+        				<Panel.Body>
+        					<div><strong><b>{ServiceHomeData.interventionsCount}</b></strong>{ServiceHomeData.textIntervention}</div>
+        				</Panel.Body>
+        				<Panel.Footer>
+        				</Panel.Footer>
+                    </Panel>
+                </Grid.Col>
+                <Grid.Col sm={4} md={3}>
+                    <Panel>
+        				<Panel.Header>
+        					Auxiliaires
+        				</Panel.Header>
+        				<Panel.Body>
+        					<div><strong><b>{ServiceHomeData.auxiliariesCount}</b></strong>{ServiceHomeData.textauxiliaries}</div>
+        				</Panel.Body>
+        				<Panel.Footer>
+        				</Panel.Footer>
+        			</Panel>
+                </Grid.Col>
+            </Grid.Row>
 		)
 	}
 
