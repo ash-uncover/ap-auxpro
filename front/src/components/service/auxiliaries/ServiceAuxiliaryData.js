b1c1c00f1748ec71798b1c158a947c2a10292367
aSH <ash.uncover@gmail.com>
2018-03-10 13:49:16 +0100

    proper show condition for auxiliary details

--- front/src/components/service/auxiliaries/ServiceAuxiliaryData.js
+++ front/src/components/service/auxiliaries/ServiceAuxiliaryData.js
@@ -4,7 +4,9 @@ import AppHelper from 'helpers/AppHelper'
 import AuxiliaryHelper from 'helpers/AuxiliaryHelper'
 import InterventionHelper from 'helpers/InterventionHelper'
 import ImageHelper from 'helpers/ImageHelper'
+import OfferHelper from 'helpers/OfferHelper'
 // utils
+import OfferStatusAux from 'utils/constants/OfferStatusAux'
 import Skills from 'utils/constants/Skills'
 // utils-lib
 import AuxiliaryUtils from 'utils-lib/entities/AuxiliaryUtils'
@@ -33,20 +35,27 @@ class ServiceAuxiliaryData extends BaseData {
 	}
 
 	_onAuxiliaryUpdate() {
-		let hasIntervention = false
-		console.log(InterventionHelper.getData())
+		let showDetails = false
 		Utils.forEach(InterventionHelper.getData(), function (intervention) {
 			if (InterventionUtils.isActive(intervention) && intervention.auxiliaryId === this.auxiliaryId) {
-				hasIntervention = true
+				showDetails = true
 			}
 		}.bind(this))
+		if (!showDetails) {
+			Utils.forEach(OfferHelper.getData(), function (offer) {
+				let intervention = InterventionHelper.getData(offer.interventionId)
+				if (offer.auxiliaryId === this.auxiliaryId && InterventionUtils.isCurrent(intervention) && offer.auxStatus === OfferStatusAux.ACCEPTED.key) {
+					showDetails = true
+				}
+			}.bind(this))
+		}
 		let auxiliary = AuxiliaryHelper.getData(this.auxiliaryId)
 		if (auxiliary) {
 			this.obj.state.avatar = auxiliary.avatar
 			this.obj.state.name = AuxiliaryUtils.getFullName(auxiliary)
-			this.obj.state.address = (hasIntervention ? auxiliary.address + ' ' : '') + (auxiliary.postalCode + ' ' + auxiliary.city)
-			this.obj.state.email = hasIntervention ? auxiliary.email : null
-			this.obj.state.phone = hasIntervention ? auxiliary.phone : null
+			this.obj.state.address = (showDetails ? auxiliary.address + ' ' : '') + (auxiliary.postalCode + ' ' + auxiliary.city)
+			this.obj.state.email = showDetails ? auxiliary.email : null
+			this.obj.state.phone = showDetails ? auxiliary.phone : null
 			this.obj.state.description = auxiliary.description
 			this.obj.state.diploma = auxiliary.diploma || []
 			for (let i = 0; i < Skills.VALUES.length; i++) {
