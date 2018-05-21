a93d988c1f27401a2fbc219bd0ec04bd5a3bf98a
aSH <ash.uncover@gmail.com>
2018-05-21 12:30:26 +0200

    tabs

--- front/src/utils-lib/entities/AuxiliaryStatusUtils.js
+++ front/src/utils-lib/entities/AuxiliaryStatusUtils.js
@@ -2,26 +2,26 @@ import AuxiliaryStatus from 'utils/constants/AuxiliaryStatus'
 
 class AuxiliaryStatusUtils {
 
-	static getName(status) {
-		switch (status) {
-			case AuxiliaryStatus.AUTO:
-			case AuxiliaryStatus.AUTO.key: return 'Je souhaite travailler pour un particulier (mandataire)' 
- 			case AuxiliaryStatus.SALARY: 
-			case AuxiliaryStatus.SALARY.key: return 'Je souhaite travailler pour une entreprise (prestataire)' 
- 			case AuxiliaryStatus.BOTH: 
-			case AuxiliaryStatus.BOTH.key: return 'Les deux' 
-		}
-		return ''
-	}
+    static getName(status) {
+        switch (status) {
+            case AuxiliaryStatus.AUTO:
+            case AuxiliaryStatus.AUTO.key: return 'Je souhaite travailler pour un particulier (mandataire)' 
+            case AuxiliaryStatus.SALARY: 
+            case AuxiliaryStatus.SALARY.key: return 'Je souhaite travailler pour une entreprise (prestataire)' 
+            case AuxiliaryStatus.BOTH: 
+            case AuxiliaryStatus.BOTH.key: return 'Les deux' 
+        }
+        return ''
+    }
 
-	static getValues() {
-		return AuxiliaryStatus.VALUES.map(function (value) {
-			return {
-				key: value.key,
-				value: AuxiliaryStatusUtils.getName(value)
-			}
-		})
-	}
+    static getValues() {
+        return AuxiliaryStatus.VALUES.map(function (value) {
+            return {
+                key: value.key,
+                value: AuxiliaryStatusUtils.getName(value)
+            }
+        })
+    }
 
     static getNameIntervention(status) {
         switch (status) {
