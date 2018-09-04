246fbd83f1ef2889630a922120ef7a80fc7caf61
I051486 <antoine.maillard@sap.com>
2018-09-04 09:04:37 +0200

    added swagger title

--- server/src/main/java/org/ap/auxpro/rest/APRestApplication.java
+++ server/src/main/java/org/ap/auxpro/rest/APRestApplication.java
@@ -36,7 +36,8 @@ public class APRestApplication extends ResourceConfig {
 		register(SwaggerSerializers.class);
 		
 		BeanConfig beanConfig = new BeanConfig();
-		beanConfig.setVersion("1.0.2");
+		beanConfig.setTitle("ap-auxpro");
+		beanConfig.setVersion("1.0.0");
 		beanConfig.setSchemes(new String[]{"http"});
 		beanConfig.setHost("localhost:8090");
 		beanConfig.setBasePath("/rest");
